422b059588bb830c6621c5ba8d58b8b49965e7a7
Idris Khan <idris.khan303@gmail.com>
2020-08-12 17:06:04 +0100

    Promises and resolved promises in NodeJS done, now moving onto await and async

--- Section-3-Async-Await/index.js
+++ Section-3-Async-Await/index.js
@@ -1,17 +1,40 @@
 const fs = require('fs');
 const superAgent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Dog Breed: ${data}`);
-  superAgent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(`Could not find Breed: ${data}`);
-      console.log(`Message: ${res.body.message}\nSuccess: ${res.body.status}`);
-
-      fs.writeFile('dog-image.txt', res.body.message, (err) => {
-        if (err) return console.log(`Could not find Breed: ${data}`);
-        console.log(`Random Dog Breed ${data} saved to file`);
-      });
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject({ readMessage: 'File Was Not Found!' });
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject({ writeMessage: 'Could Write File' });
+      resolve('Success');
+    });
+  });
+};
+
+readFilePromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Dog Breed: ${data}`);
+    return superAgent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .catch((readMessage) => {
+    console.log(readMessage);
+  })
+  .then((res) => {
+    console.log(`Message: ${res.body.message}\nSuccess: ${res.body.status}`);
+    return writeFilePromise('dog-image.txt', res.body.message);
+  })
+  .catch((writeMessage) => {
+    console.log(writeMessage);
+  })
+  .then(() => {
+    console.log(`Random Dog Breed saved to file`);
+  })
+  .catch(() => console.log('Something went wrong'));
