400a25dda4975f909b703c7925ea68371b65f6a2
Idris Khan <idris.khan303@gmail.com>
2020-09-24 17:37:04 +0100

    Created Review Model

--- Section-4-Natours/models/tourModel.js
+++ Section-4-Natours/models/tourModel.js
@@ -187,6 +187,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -307,6 +313,14 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// tourSchema.pre('save', async function (next) {
+//   // const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   // this.guides = await Promise.all(guidesPromises);
+
+//   this.guides = await Promise.all(this.guides.map((id) => User.findById(id)));
+//   next();
+// });
+
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document...');
 //   next();
@@ -323,6 +337,14 @@ tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
 
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/i, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
   next();
 });
 
