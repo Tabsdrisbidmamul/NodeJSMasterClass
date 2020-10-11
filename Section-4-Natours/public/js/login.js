58937e7198e57be29ee7e55eab60dd2c8ed6ea4c
Idris Khan <idris.khan303@gmail.com>
2020-10-11 19:59:38 +0100

    Finish the lecture on polyfill

--- Section-4-Natours/public/js/login.js
+++ Section-4-Natours/public/js/login.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 
-const login = async (email, password) => {
+import { axios } from 'axios';
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -24,10 +26,3 @@ const login = async (email, password) => {
     alert(message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
